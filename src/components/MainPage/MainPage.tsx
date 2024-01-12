c7ce689861dfe482001be2a9b25964d89efb492c
Geun-Oh <kandy1002@naver.com>
2024-01-12 01:32:42 +0100

    test: testing createRoom, message interaction

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,10 +1,12 @@
 // src/MainPage.js
 import React, { useEffect } from "react";
 import styled from "styled-components";
-import { doc, onSnapshot } from "firebase/firestore";
+import { collection, doc, onSnapshot, query, where } from "firebase/firestore";
 import { firestore } from "../../firebase";
 import { useSelector } from "react-redux";
 import { User } from "../../types/type";
+import { Link } from "react-router-dom";
+import { addRoomToChatList, createRoom } from "../../api/api";
 
 const MainPageContainer = styled.div`
   max-width: 800px;
@@ -20,23 +22,51 @@ const MainPage = () => {
   const user = useSelector((state: any) => state.auth.user as User);
 
   useEffect(() => {
-    const unsub = onSnapshot(doc(firestore, "User", user.uid), (doc) => {
-      console.log("Current data: ", doc.data());
+    if (!user) return;
+    const q = query(
+      collection(firestore, "User"),
+      where("uid", "==", user.uid)
+    );
+    const unsub = onSnapshot(q, (snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log("Current data: ", doc.data());
+      });
     });
     return () => {
       unsub();
     };
-  }, []);
+  }, [user]);
 
   return (
     <MainPageContainer>
       <h1>Main Page</h1>
       <Section>
         <h2>Contacts</h2>
+        <button
+          type="button"
+          onClick={async () => {
+            const roomRef = await createRoom([
+              doc(firestore, "User", user.uid),
+              doc(firestore, "User", "tqwovhIKgKNTWJLg3wEW1472L1C3"),
+            ]);
+            await addRoomToChatList(
+              [
+                doc(firestore, "User", user.uid),
+                doc(firestore, "User", "tqwovhIKgKNTWJLg3wEW1472L1C3"),
+              ],
+              roomRef
+            );
+          }}
+        >
+          Create!
+        </button>
         {/* Add contacts list or component here */}
       </Section>
       <Section>
         <h2>Messages</h2>
+        <Link to={`/chat/${user.uid}/7xjwa7dmCHCfAjnW9Kke`}>
+          Go to test room
+        </Link>
         {/* Add messages list or component here */}
       </Section>
       <Section>
