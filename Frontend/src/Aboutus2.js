30d4f42122ffaca16a7f7b60cba2ed2fd5d9af57
Utkarshmhatre <utkarshmhatre434@gmail.com>
2024-02-22 21:54:28 +0530

    about us page restructured and added images

--- Frontend/src/Aboutus2.js
+++ Frontend/src/Aboutus2.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './global.css';
 import './Aboutus2.css';
+import logo from './imagescontext';
+import { CollectionReference } from '@firebase/firestore';
 
 const AboutUs2 = () => {
   return (
+
+    
     <div className="about-us-2">
       <section className="frame-project-inspiration">
         <div className="frame-cultural-xchange-text">
@@ -28,119 +32,122 @@ const AboutUs2 = () => {
                 </span>
               </b>
             </div>
+           
           </header>
         </div>
-        <div className="frame-design-team-members">
-          <div className="ellipse-rushabh-dorgeai">
-            <div className="slice-vector-vector">
-              <div className="mask-group-frame-components"></div>
-              <div className="rectangle-container">
-                <div className="frame-child1"></div>
-                <div className="priyant-a-passionate-container">
-                  <p className="blank-line1">&nbsp;</p>
-                  <p className="priyant-a-passionate">
-                    Priyant, a passionate cultural enthusiast, established
-                    Bridging
-                  </p>
-                  <p className="worlds-to-break">
-                    Worlds to break down barriers and promote unity.
-                  </p>
-                  <p className="his-dedication-to">
-                    His dedication to promoting mutual respect and appreciation
-                  </p>
-                  <p className="serves-as-the-driving-force-be">
-                    <span className="serves-as-the">
-                      serves as the driving force behind our endeavor.
-                    </span>
-                    <span className="span"> </span>
-                  </p>
-                </div>
-              </div>
-            </div>
-            <b className="priyant-banerjee-president-container">
-              <span className="priyant-banerjee-president-container1">
-                <p className="priyant-banerjee">priyant Banerjee.</p>
-                <p className="president">president.</p>
-              </span>
-            </b>
-          </div>
+        </section>
+            
+
+        
+        <div class="about-us-3"> 
+      <div class="about-us-3-child"></div>
+      <div class="about-us-3-item"></div>
+      <div class="about-us-3-inner"></div>
+      <div class="slice-div"></div>
+      <div class="ellipse-group">
+        <img class="ellipse-icon" alt="" src={logo['priyant']} />
+
+        <b class="priyant-banerjee-president-container2">
+          <span class="priyant-banerjee-president-container3">
+            <p class="priyant-banerjee1">priyant Banerjee.</p>
+            <p class="president1">president.</p>
+          </span>
+        </b>
+      </div>
+      <div class="ellipse-container">
+        <img class="group-child" alt="" src={logo['Hyung']} />
+
+        <b class="hyeonggeun-oh-main-container1">
+          <span class="hyeonggeun-oh-main-container2">
+            <p class="hyeonggeun-oh1">hyeonggeun oh.</p>
+            <p class="main-developer1">main developer.</p>
+          </span>
+        </b>
+      </div>
+      <div class="ellipse-parent1">
+        <img class="group-item" alt="" src={logo['rushab']} />
+
+        <b class="rushabh-dorage-ai-container1">
+          <span class="rushabh-dorage-ai-container2">
+            <p class="rushabh-dorage1">rushabh dorage.</p>
+            <p class="ai-lead1">ai lead.</p>
+          </span>
+        </b>
+      </div>
+      <div class="ellipse-parent2">
+        <img class="group-inner" alt="" src={logo['bhavesh']}/>
+
+        <b class="bhavesh-ahirrao-director-container1">
+          <span class="bhavesh-ahirrao-director-container2">
+            <p class="bhavesh-ahirrao1">Bhavesh Ahirrao.</p>
+            <p class="director-of-designs1">director of designs.</p>
+          </span>
+        </b>
+      </div>
+      <div class="priyant-a-passionate-cultural-wrapper">
+        <div class="priyant-a-passionate-container1">
+          <p class="blank-line3">&nbsp;</p>
+          <p class="priyant-a-passionate1">
+            Priyant, a passionate cultural enthusiast, established Bridging
+          </p>
+          <p class="worlds-to-break1">
+            Worlds to break down barriers and promote unity.
+          </p>
+          <p class="his-dedication-to1">
+            His dedication to promoting mutual respect and appreciation
+          </p>
+          <p class="serves-as-the-driving-force-be1">
+            <span class="serves-as-the1"
+              >serves as the driving force behind our endeavor.</span
+            >
+            <span class="span1"> </span>
+          </p>
         </div>
-        <div className="rushabh-dorge-slice"></div>
-      </section>
-      <section className="frame-bhavesh-ahirrao">
-        <div className="group-div">
-          <div className="frame-child2"></div>
-          <div className="bhavesh-ahirrao-a">
-            Bhavesh Ahirrao, a dynamic design director, founded Artistry Fusion
-            with a fervent dedication to pushing creative boundaries and
-            fostering innovation. His unwavering commitment to cultivating
-            artistic excellence propels our mission forward, inspiring
-            collaboration and pushing the limits of design possibilities.
-          </div>
+      </div>
+      <div class="rectangle-parent3">
+        <div class="group-child1"></div>
+        <div class="bhavesh-ahirrao-a1">
+          Bhavesh Ahirrao, a dynamic design director, founded Artistry Fusion
+          with a fervent dedication to pushing creative boundaries and fostering
+          innovation. His unwavering commitment to cultivating artistic
+          excellence propels our mission forward, inspiring collaboration and
+          pushing the limits of design possibilities.
         </div>
-        <div className="frame-group-cultural-xchange">
-          <div className="ellipse-priyant-banerjee"></div>
-          <b className="bhavesh-ahirrao-director-container">
-            <span>
-              <p className="bhavesh-ahirrao">Bhavesh Ahirrao.</p>
-              <p className="director-of-designs">director of designs.</p>
-            </span>
-          </b>
+      </div>
+      <div class="rectangle-parent4">
+        <div class="group-child2"></div>
+        <div class="hyeonggeun-oh-a1">
+          Hyeonggeun Oh, a driven main developer, spearheaded TechGenius with a
+          steadfast commitment to pushing technological boundaries and driving
+          innovation. His relentless dedication to fostering excellence propels
+          our team forward, inspiring collaboration and pushing the limits of
+          technological possibilities.
         </div>
-        <div className="slice-design-frames"></div>
-      </section>
-      <section className="frame-bhavesh-ahirrao1">
-        <div className="ellipse-parent">
-          <div className="ellipse-div"></div>
-          <b className="hyeonggeun-oh-main-container">
-            <span>
-              <p className="hyeonggeun-oh">hyeonggeun oh.</p>
-              <p className="main-developer">main developer.</p>
-            </span>
-          </b>
+      </div>
+      <div class="rectangle-parent5">
+        <div class="group-child3"></div>
+        <div class="rushabh-dorage-a-container1">
+          <span class="rushabh-dorage-a-container2">
+            <p class="blank-line4">&nbsp;</p>
+            <p class="rushabh-dorage-a1">
+              Rushabh Dorage, a pioneering AI lead, established Tech Innovate to
+              spearhead advancements in artificial intelligence and drive
+              transformative change. His relentless pursuit of cutting-edge
+              solutions fuels our collective vision, inspiring breakthroughs and
+              pushing the boundaries of technological innovation.
+            </p>
+          </span>
         </div>
-        <div className="rectangle-parent1">
-          <div className="frame-child3"></div>
-          <div className="hyeonggeun-oh-a">
-            Hyeonggeun Oh, a driven main developer, spearheaded TechGenius with
-            a steadfast commitment to pushing technological boundaries and
-            driving innovation. His relentless dedication to fostering
-            excellence propels our team forward, inspiring collaboration and
-            pushing the limits of technological possibilities.
-          </div>
-        </div>
-        <div className="frame-bhavesh-ahirrao-child"></div>
-      </section>
-      <section className="image-vector-frame">
-        <div className="a-i-group-frame">
-          <div className="rectangle-parent2">
-            <div className="frame-child4"></div>
-            <div className="rushabh-dorage-a-container">
-              <span>
-                <p className="blank-line2">&nbsp;</p>
-                <p className="rushabh-dorage-a">
-                  Rushabh Dorage, a pioneering AI lead, established Tech
-                  Innovate to spearhead advancements in artificial intelligence
-                  and drive transformative change. His relentless pursuit of
-                  cutting-edge solutions fuels our collective vision, inspiring
-                  breakthroughs and pushing the boundaries of technological
-                  innovation.
-                </p>
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="ellipse-rushabh-dorge-parent">
-          <div className="ellipse-rushabh-dorge"></div>
-          <b className="rushabh-dorage-ai-container">
-            <span>
-              <p className="rushabh-dorage">rushabh dorage.</p>
-              <p className="ai-lead">ai lead.</p>
-            </span>
-          </b>
-        </div>
-        <div className="image-vector-frame-child"></div>
-      </section>
+      </div>
+    </div>
+       
+
+
+
+
+
+
+
       <section className="about-us-2-inner">
         <div className="a-m-d-jpeg-intel-p-n-g-wrapper">
           <div className="a-m-d-jpeg-intel-p-n-g">
@@ -150,76 +157,76 @@ const AboutUs2 = () => {
               <img
                 className="x-jpeg"
                 loading="eager"
-                alt=""
-                src="./public/x--jpeg@2x.png"
+                alt="twitter png"
+                src={logo['twitter']}
               />
 
               <img
                 className="g12-icon"
                 loading="eager"
-                alt=""
-                src="./public/g12.svg"
+                alt="facebook"
+                src={logo['facebook']}
               />
 
               <img
                 className="mask-group-icon"
                 loading="eager"
-                alt=""
-                src="./public/mask-group.svg"
+                alt="vscode"
+                src={logo['vscode']}
               />
 
               <img
                 className="vector-rectangle-icon"
-                alt=""
-                src="./public/vector-rectangle.svg"
+                alt="error"
+                src={logo['vector-rectangle']}
               />
 
               <img
                 className="vector-rectangle-icon1"
-                alt=""
-                src="./public/vector-1.svg"
+                alt="ts"
+                src={logo['ts']}
               />
 
               <img
                 className="image-2-icon"
                 loading="eager"
-                alt=""
-                src="./public/image-2@2x.png"
+                alt="reactjs"
+                src={logo['reactjs']}
               />
 
               <img
                 className="image-3-icon"
                 loading="eager"
-                alt=""
-                src="./public/image-3@2x.png"
+                alt="css"
+                src={logo['css']}
               />
             </div>
             <img
               className="amd-jpeg"
               loading="eager"
-              alt=""
-              src="./public/amd--jpeg@2x.png"
+              alt="amd"
+              src={logo['amd']}
             />
 
             <img
               className="a-m-d-jpeg-intel-p-n-g-child"
               loading="eager"
-              alt=""
-              src="./public/group-50.svg"
+              alt="google"
+              src={logo['google']}
             />
 
             <img
               className="intel-png"
               loading="eager"
-              alt=""
-              src="./public/intel--png@2x.png"
+              alt="intel"
+              src={logo['intel']}
             />
 
             <img
               className="figma-svg-icon"
               loading="eager"
-              alt=""
-              src="./public/figma-svg.svg"
+              alt="figma"
+              src={logo['figma']}
             />
           </div>
         </div>
@@ -233,6 +240,7 @@ const AboutUs2 = () => {
         </div>
       </div>
     </div>
+    
   );
 };
 
